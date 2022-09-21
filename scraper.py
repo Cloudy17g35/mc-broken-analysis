42229c4887f26b37a46ef6d1af6e2fd9680ca792
Ksawery Lejczak <ksawery.lejczak@gmail.com>
2022-09-21 14:54:28 +0200

    adding scraper

--- scraper.py
+++ scraper.py
@@ -1,19 +1,19 @@
-# this script scrapes currently broken ice-machines in McDonalds in US States https://mcbroken.com/
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
 
-import requests
-URL:str = 'https://mcbroken.com/'
+
+def get_percentage_of_currently_broken_machines(url:str) -> str:
+    browser = webdriver.Firefox()  
+    browser.get(url)
+    sleep(2)
+    stats = browser.find_element(
+    By.CSS_SELECTOR,
+    'div.stats-list'
+    )
+    percentage_of_broken:str = stats.find_element(
+        By.CSS_SELECTOR,
+        'p.stats-row-broken').text
+    return percentage_of_broken
 
 
-def make_request(url:str) -> requests.Response:
-    response:requests.Response = requests.get(url)
-    return response
-
-def main():
-    response:requests.Response = make_request(URL)
-    if not response.ok:
-        print(f'Invalid response status code: {response.status_code}')
-        
-    
-
-if __name__ == '__main__':
-    main()
